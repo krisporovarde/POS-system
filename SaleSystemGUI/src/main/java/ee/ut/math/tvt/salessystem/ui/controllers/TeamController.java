06e9781425319ef1244b736c168ef3a30f9a7659
tarvitamm <tarvi.tamm@gmail.com>
2022-10-10 19:16:20 +0300

    Half done with the team tab

---
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
@@ -0,0 +1,66 @@
+package ee.ut.math.tvt.salessystem.ui.controllers;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.Set;
+
+public class TeamController {
+    private String teamName;
+    private String contactPerson;
+    private String[] teamMembers;
+    private String teamLogo;
+
+    public TeamController() throws FileNotFoundException {
+        this.teamName = teamName;
+        this.contactPerson = contactPerson;
+        this.teamMembers = teamMembers;
+        this.teamLogo = teamLogo;
+    }
+
+    //private static final Properties configProp = new Properties();
+
+    /*private static void PropertiesCache() {
+        //Private constructor to restrict new instances
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("application.properties");
+        System.out.println("Reading all properties from the file");
+        try {
+            configProp.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+    public static void main(String args[]) throws FileNotFoundException {
+
+    }
+    public void ReadInto() throws FileNotFoundException {
+        TeamController readingProp = new TeamController();
+        readingProp.ReadProperties();
+        teamName = readingProp.teamName;
+        teamMembers = readingProp.teamMembers;
+        contactPerson = readingProp.contactPerson;
+        System.out.println(teamName);
+    }
+
+    public void ReadProperties() {
+        Properties properties = new Properties();
+        try (final InputStream inputStream = this.getClass().getResourceAsStream("../application.properties")) {
+            properties.load(inputStream);
+            teamName = properties.getProperty(teamName);
+            System.out.println(teamName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+}
