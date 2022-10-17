2039c21c589b6ef0797b6380930a87b31a3bf51d
krisporovarde <kris.porovarde@gmail.com>
2022-10-18 00:01:46 +0300

    Homework 3

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.apache.logging.log4j.core.config.properties.PropertiesConfiguration;
 
 import java.io.*;
 import java.net.URL;
@@ -23,62 +24,30 @@ public class TeamController implements Initializable {
     @FXML
     private TextArea textArea;
 
-    public TeamController() throws FileNotFoundException {
-        this.teamName = teamName;
-        this.contactPerson = contactPerson;
-        this.teamMembers = teamMembers;
-        this.teamLogo = teamLogo;
-    }
 
-    //private static final Properties configProp = new Properties();
-
-    /*private static void PropertiesCache() {
-        //Private constructor to restrict new instances
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream("application.properties");
-        System.out.println("Reading all properties from the file");
-        try {
-            configProp.load(in);
+    public static Properties ReadProperties(String filename) throws Exception{
+        Properties teaminfo = new Properties();
+        try  {
+            FileInputStream file = new FileInputStream(filename);
+            teaminfo.load(file);
+            file.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }*/
-//    public static void main(String args[]) throws FileNotFoundException {
-//
-//    }
-//    public void ReadInto() throws FileNotFoundException {
-//        TeamController readingProp = new TeamController();
-//        readingProp.ReadProperties();
-//        teamName = readingProp.teamName;
-//        teamMembers = readingProp.teamMembers;
-//        contactPerson = readingProp.contactPerson;
-//        System.out.println(teamName);
-//    }
-//
-//    public void ReadProperties() throws Exception{
-//        Properties teaminfo = new Properties();
-//        try (final InputStream inputStream = this.getClass().getResourceAsStream("src/main/resources/application.properties")) {
-//            teaminfo.load(inputStream);
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-////        teamName = teaminfo.getProperty("teamName");
-////        contactPerson = teaminfo.getProperty("contactPerson");
-////        teamMembers = teaminfo.getProperty("teamMembers");
-////        teamLogo = teaminfo.getProperty("teamLogo");
-//        System.out.println(teaminfo);
-//
-//    }
+        return teaminfo;
+
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        File file = new File("../../../../../../../../../../resources/LV.png");
-//        Image image = new Image(file.toURI().toString());
-//        imageView.setImage(image);
-//        try {
-//            ReadProperties();
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        textArea.setText(teamName + "\n" + contactPerson + "\n" + teamMembers + "\n" + teamLogo);
+        try {
+            Properties teaminfo = ReadProperties("../application.properties");
+            this.teamName = teaminfo.getProperty("teamName");
+            this.contactPerson = teaminfo.getProperty("contactPerson");
+            this.teamMembers = teaminfo.getProperty("teamMembers");
+            //textArea.setText(teamName + "\n" + contactPerson + "\n" + teamMembers);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
